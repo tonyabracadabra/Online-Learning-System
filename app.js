24a7299010e6f81b2e1bb0a6753bdf3d4a9634a3
Xupeng Tong <tongxupeng.cpu@gmail.com>
2017-02-23 14:10:11 -0500

    add layout

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var exphbs = require('express-handlebars');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var flash = require('flash');
+var mongo = require('mongodb');
+var mogoose = require('mongoose');
+// passport local strategy 
+var LocalStrategy = require('passport-local'), Strategy;
+
+// let it be default 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+// use the folder views
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +34,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Express session
+// The session is then protected against session hijacking by checking the 
+// fingerprint against the hash with the secret:
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// from the github page
+// Express validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while (namesapce.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param : formParam,
+			msg : msg,
+			value : value
+		};
+	}
+}));
+
+// Connect flash
+app.use(flash());
+
+// Create Global vars called messages
+app.use(function (req, res, next) {
+	res.locals.messages = require('express-messages')(req, res);
+	next();
+});
 
 app.use('/', index);
 app.use('/users', users);
